2e278e343eb4238ad8e5f962c41fc50ce5783226
sharadha507 <161309099+sharadha507@users.noreply.github.com>
2024-03-05 21:43:23 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,25 @@
+s={1,2,2,3,4,4}
+x=list(s)
+print(x[2])
+x="22_22"
+print(float(x))
+print(0.1+0.2==0.3)
+x=2,
+y=2
+print(type(x)==type(y))
+x=1#2#3
+print(x)
+from turtle import*
+speed(10)
+color('red')
+bgcolor('black')
+b=200
+while b>0:
+  left(b)
+  forward(b*3)
+  b=b-1
+  fruits="7"
+  fruits=fruits+"0"
+  eggs=int(fruits)+3
+  print(float(eggs))
+
