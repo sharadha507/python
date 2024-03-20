63429e0ebefe051793f9e3f60d6b46bf31c60d59
sharadha507 <161309099+sharadha507@users.noreply.github.com>
2024-03-20 21:29:40 +0530

    Add files via upload

---
+++ CommandCopy.py
@@ -0,0 +1,15 @@
+from sys import argv
+if len(argv)==4:
+    try:
+        fp1=open(argv[1],"r")
+        fp2=open(argv[2],"w+")
+        for i in fp1:
+            fp2.write(i)
+        print("file is copies successfully")
+        fp2.seek(0,0)
+        cont=fp2.read()
+        print(cont)
+        fp1.close()
+        fp2.close()
+    except Exception:
+        print("error in copy")
